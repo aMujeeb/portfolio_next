102b12128fb1b21fed8afa71ff6c4ef71af0a004
Mujee <mujee2@gmail.com>
2026-04-11 22:08:16 +0800

    Add Education

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,6 +1,18 @@
+'use client';
+import EducationItem from "../components/educationItem";
+import { useLanguage } from "../context/LanguageContext";
+
 const Education = () =>  {
+    const { t } = useLanguage();
+
     return (
-        <div>I am the Education Page</div>
+        <div className="p-2 flex flex-col justify-start gap-4">
+            <p className="text-3xl font-bold text-amber-400">{t.education.title}</p>
+            <p className="text-xl font-bold text-amber-600">{t.education.viewFullEducationHistory}</p>
+            {t.education.educationHistory.map((educationItem: any) => (
+                <EducationItem key={educationItem.id} educationItem={educationItem} />
+            ))}
+        </div>
     );
 }
 
